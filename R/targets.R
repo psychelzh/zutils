713d611f8569b5ec78b5544bdcf764a953399470
Liang Zhang <psychelzh@outlook.com>
2024-01-05 18:39:52 +0800

    Add `tar_combine_with_meta()`
    Signed-off-by: Liang Zhang <psychelzh@outlook.com>

---
+++ R/targets.R
@@ -0,0 +1,45 @@
+#' Static aggregation with metadata
+#'
+#' Like [tarchetypes::tar_combine()] except it adds metadata columns to the
+#' combined data frame.
+#'
+#' This function is useful for combining data frames. For [list()] targets,
+#' you should pass a function to convert the list to a data frame in the
+#' `fun_pre` argument. Metadata is retrieved from the names of the targets.
+#'
+#' @param name Name of the target. Could be a string or symbol.
+#' @param cols_meta Character vector of column names to store the retrieved
+#'   metadata.
+#' @param ... One or more target objects or list of target objects. See
+#'   [tarchetypes::tar_combine()].
+#' @param prefix Prefix to be removed from the target names to retrieve the
+#'   metadata. Defaults is the same as `name`.
+#' @param fun_pre Function to apply to each target before combining.
+#' @param fun_post Function to apply to the combined data frame.
+#' @return A target object. See [tarchetypes::tar_combine()].
+#' @export
+tar_combine_with_meta <- function(name, cols_meta, ...,
+                                  prefix = NULL,
+                                  fun_pre = identity,
+                                  fun_post = identity) {
+  rlang::check_dots_used()
+  ischar_name <- tryCatch(
+    is.character(name) && length(name) == 1L,
+    error = function(e) FALSE
+  )
+  if (!ischar_name) name <- deparse1(substitute(name))
+  if (is.null(prefix)) prefix <- name
+  tarchetypes::tar_combine_raw(
+    name,
+    ...,
+    command = bquote(
+      list(!!!.x) |>
+        lapply(.(rlang::as_function(fun_pre))) |>
+        bind_rows(.id = "id") |>
+        # note there is delimiter after prefix should be removed too
+        mutate(id = str_remove(id, str_c(.(prefix), "."))) |>
+        separate(id, .(cols_meta), convert = TRUE) |>
+        .(rlang::as_function(fun_post))()
+    )
+  )
+}
